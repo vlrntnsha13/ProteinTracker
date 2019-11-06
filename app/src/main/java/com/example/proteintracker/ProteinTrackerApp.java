72bd44a23bab59572f4ef01cff3e7a09db31d2ad
vlrntnsha13 <valeriana.tanesha@si.ukdw.ac.id>
2019-11-06 19:06:46 +0700

    Tugas Setting dan App Preferences

--- app/src/main/java/com/example/proteintracker/ProteinTrackerApp.java
+++ app/src/main/java/com/example/proteintracker/ProteinTrackerApp.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +21,50 @@ public class ProteinTrackerApp extends AppCompatActivity {
         Button resetButton = (Button)findViewById(R.id.resetBtn);
         resetButton.setOnClickListener(resetButtonListener);
 
+        Button btnSetting = (Button)findViewById(R.id.btnSetting);
+        btnSetting.setOnClickListener(myBtnSettingClick);
+
+        SharedPreferences prefs = ProteinTrackerApp.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+        String statusLogin = prefs.getString("isLogin",null);
+        Button btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(myBtnLoginClick);
+        if (statusLogin != null){
+            btnLogin.setText("Logout");
+        }else{
+            btnLogin.setText("Login");
+        }
+
+
     }
+
+    private View.OnClickListener myBtnLoginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SharedPreferences prefs = ProteinTrackerApp.this.getSharedPreferences("prefs_file",MODE_PRIVATE);
+            String statusLogin = prefs.getString("isLogin",null);
+            SharedPreferences.Editor edit = prefs.edit();
+            Button btnLogin = (Button)findViewById(R.id.btnLogin);
+            if (statusLogin != null){
+                edit.putString("isLogin",null);
+                btnLogin.setText("Login");
+            }else{
+                edit.putString("isLogin","Admin");
+                btnLogin.setText("Logout");
+            }
+            edit.commit();
+        }
+    };
+
+
+    private View.OnClickListener myBtnSettingClick = new
+            View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(ProteinTrackerApp.this,
+                            SettingsActivity.class);
+                    startActivity(intent);
+                };
+            };
 
     private View.OnClickListener resetButtonListener = new View.OnClickListener() {
         @Override
